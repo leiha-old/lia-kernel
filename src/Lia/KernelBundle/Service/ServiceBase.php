04344fc0723a19f06512a1a032abb059a75a3d2a
leiha <leiha.sellier@gmail.com>
2015-03-27 21:56:58 +0100

    Automatise the get of path of asset

--- src/Lia/KernelBundle/Service/ServiceBase.php
+++ src/Lia/KernelBundle/Service/ServiceBase.php
@@ -16,11 +16,26 @@ abstract class ServiceBase
     protected $config;
 
     /**
+     * @var string
+     */
+    private $bundleName;
+
+    /**
      * @param ContainerInterface $container
      */
     public function __construct(ContainerInterface $container)
     {
         $this->setContainer($container);
+    }
+
+    protected function getBundleName()
+    {
+        if(!$this->bundleName) {
+            $c = get_called_class();
+            $this->bundleName = substr($c, 0, strpos($c, 'Bundle\\'));
+            $this->bundleName = str_replace('\\', '', $this->bundleName);
+        }
+        return $this->bundleName;
     }
 
     /**
